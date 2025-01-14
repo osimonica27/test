bc1a0a38b8121f48b177e6c80eb7d1b737abf74a
fengmk2 <fengmk2@gmail.com>
2025-01-14 14:20:30 +0800

    feat(server): workspace model

--- packages/backend/server/src/models/index.ts
+++ packages/backend/server/src/models/index.ts
@@ -3,19 +3,27 @@ import { Global, Injectable, Module } from '@nestjs/common';
 import { SessionModel } from './session';
 import { UserModel } from './user';
 import { VerificationTokenModel } from './verification-token';
+import { WorkspaceModel } from './workspace';
 
 export * from './session';
 export * from './user';
 export * from './verification-token';
+export * from './workspace';
 
-const models = [UserModel, SessionModel, VerificationTokenModel] as const;
+const models = [
+  UserModel,
+  SessionModel,
+  VerificationTokenModel,
+  WorkspaceModel,
+] as const;
 
 @Injectable()
 export class Models {
   constructor(
     public readonly user: UserModel,
     public readonly session: SessionModel,
-    public readonly verificationToken: VerificationTokenModel
+    public readonly verificationToken: VerificationTokenModel,
+    public readonly workspace: WorkspaceModel
   ) {}
 }
 
