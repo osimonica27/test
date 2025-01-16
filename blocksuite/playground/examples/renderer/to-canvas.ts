d203174cdbd02697ba6468ba2705f9dca8f9421e
Yifeng Wang <doodlewind@toeverything.info>
2025-01-16 11:24:38 +0800

    refactor: code stucture

--- blocksuite/playground/examples/renderer/to-canvas.ts
+++ blocksuite/playground/examples/renderer/to-canvas.ts
@@ -1,3 +1,5 @@
+import type { EditorHost } from '@blocksuite/block-std';
+
 import { type ParagraphData } from './types.js';
 
 let worker: Worker | null = null;
@@ -14,58 +16,61 @@ function initWorker(width: number, height: number) {
   return worker;
 }
 
-export function initToCanvasSync() {
-  const toCanvasButton = document.querySelector('#to-canvas-button')!;
-  toCanvasButton.addEventListener('click', () => {
-    const editorHost = document.querySelector('editor-host')!;
-    const rect = editorHost.getBoundingClientRect();
-    const { width, height } = rect;
-
-    const rightColumn = document.querySelector('#right-column')!;
-
-    const worker = initWorker(width, height);
-    const paragraphs = editorHost.querySelectorAll('affine-paragraph');
-    const editorRect = editorHost.getBoundingClientRect();
-
-    const paragraphData: ParagraphData[] = Array.from(paragraphs).map(p => {
-      const rect = p.getBoundingClientRect();
-      const computedStyle = window.getComputedStyle(p);
-      return {
-        rect,
-        text: p.textContent || '',
-        style: {
-          fontSize: computedStyle.fontSize,
-          fontFamily: computedStyle.fontFamily,
-          lineHeight: computedStyle.lineHeight,
-        },
-      };
-    });
-    worker.postMessage({
-      type: 'draw',
-      data: {
-        paragraphs: paragraphData,
-        editorRect,
+function getWorkerData(editorHost: EditorHost) {
+  const paragraphBlocks = editorHost.querySelectorAll('affine-paragraph');
+  const paragraphs: ParagraphData[] = Array.from(paragraphBlocks).map(p => {
+    const rect = p.getBoundingClientRect();
+    const computedStyle = window.getComputedStyle(p);
+    return {
+      rect,
+      text: p.textContent || '',
+      style: {
+        fontSize: computedStyle.fontSize,
+        fontFamily: computedStyle.fontFamily,
+        lineHeight: computedStyle.lineHeight,
       },
-    });
-
-    worker.onmessage = (e: MessageEvent) => {
-      const { type, bitmap } = e.data;
-      if (type === 'render') {
-        const existingCanvas = rightColumn.querySelector('canvas');
-        if (existingCanvas) {
-          existingCanvas.remove();
-        }
-
-        const canvas = document.createElement('canvas');
-        canvas.style.width = '100%';
-        canvas.style.height = '100%';
-        canvas.width = width * window.devicePixelRatio;
-        canvas.height = height * window.devicePixelRatio;
-        rightColumn.append(canvas);
-
-        const ctx = canvas.getContext('bitmaprenderer');
-        ctx?.transferFromImageBitmap(bitmap);
-      }
     };
   });
+  const editorRect = editorHost.getBoundingClientRect();
+  return { paragraphs, editorRect };
+}
+
+function toCanvas() {
+  const editorHost = document.querySelector('editor-host')!;
+  const { paragraphs, editorRect } = getWorkerData(editorHost);
+  const { width, height } = editorRect;
+  const worker = initWorker(width, height);
+  worker.postMessage({
+    type: 'draw',
+    data: {
+      paragraphs,
+      editorRect,
+    },
+  });
+
+  worker.onmessage = (e: MessageEvent) => {
+    const { type, bitmap } = e.data;
+    if (type === 'render') {
+      const rightColumn = document.querySelector('#right-column')!;
+      const existingCanvas = rightColumn.querySelector('canvas');
+      if (existingCanvas) {
+        existingCanvas.remove();
+      }
+
+      const canvas = document.createElement('canvas');
+      canvas.style.width = '100%';
+      canvas.style.height = '100%';
+      canvas.width = width * window.devicePixelRatio;
+      canvas.height = height * window.devicePixelRatio;
+      rightColumn.append(canvas);
+
+      const ctx = canvas.getContext('bitmaprenderer');
+      ctx?.transferFromImageBitmap(bitmap);
+    }
+  };
+}
+
+export function initToCanvasSync() {
+  const toCanvasButton = document.querySelector('#to-canvas-button')!;
+  toCanvasButton.addEventListener('click', toCanvas);
 }
