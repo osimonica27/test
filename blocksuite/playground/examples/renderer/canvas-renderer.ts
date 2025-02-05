4f271ed1de9a5bef36586ca9cf272a1f8485cd7c
Yifeng Wang <doodlewind@toeverything.info>
2025-02-06 00:21:43 +0800

    refactor: remove hard coded animator

--- blocksuite/playground/examples/renderer/canvas-renderer.ts
+++ blocksuite/playground/examples/renderer/canvas-renderer.ts
@@ -4,7 +4,6 @@ import type { AffineEditorContainer } from '@blocksuite/presets';
 import { getSentenceRects, segmentSentences } from './text-utils.js';
 import {
   type ParagraphLayout,
-  type Rect,
   type SectionLayout,
   type ViewportState,
 } from './types.js';
@@ -182,104 +181,6 @@ export class CanvasRenderer {
         }
       };
     });
-  }
-
-  public renderTransitionFrame(
-    beginSection: SectionLayout,
-    endSection: SectionLayout,
-    beginHostRect: Rect,
-    endHostRect: Rect,
-    progress: number
-  ) {
-    const editorContainerRect = this.editorContainer.getBoundingClientRect();
-    const dpr = window.devicePixelRatio;
-
-    if (!this.targetContainer.querySelector('canvas')) {
-      this.targetContainer.append(this.canvas);
-    }
-
-    const ctx = this.canvas.getContext('2d')!;
-    ctx.scale(dpr, dpr);
-    ctx.clearRect(0, 0, this.canvas.width / dpr, this.canvas.height / dpr);
-
-    const getParagraphRect = (paragraph: ParagraphLayout): Rect => {
-      let minX = Infinity;
-      let minY = Infinity;
-      let maxX = -Infinity;
-      let maxY = -Infinity;
-      paragraph.sentences.forEach(sentence => {
-        sentence.rects.forEach(({ rect }) => {
-          minX = Math.min(minX, rect.x);
-          minY = Math.min(minY, rect.y);
-          maxX = Math.max(maxX, rect.x + rect.w);
-          maxY = Math.max(maxY, rect.y + rect.h);
-        });
-      });
-
-      return {
-        x: minX,
-        y: minY,
-        w: maxX - minX,
-        h: maxY - minY,
-      };
-    };
-
-    // Helper function to interpolate between two rects
-    const interpolateRect = (rect1: Rect, rect2: Rect, t: number): Rect => {
-      return {
-        x: rect1.x + (rect2.x - rect1.x) * t,
-        y: rect1.y + (rect2.y - rect1.y) * t,
-        w: rect1.w + (rect2.w - rect1.w) * t,
-        h: rect1.h + (rect2.h - rect1.h) * t,
-      };
-    };
-
-    // Draw host rect
-    const currentHostRect = interpolateRect(
-      beginHostRect,
-      endHostRect,
-      progress
-    );
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 1;
-    ctx.strokeRect(
-      currentHostRect.x - editorContainerRect.x,
-      currentHostRect.y - editorContainerRect.y,
-      currentHostRect.w,
-      currentHostRect.h
-    );
-
-    // Draw paragraph rects
-    const maxParagraphs = Math.max(
-      beginSection.paragraphs.length,
-      endSection.paragraphs.length
-    );
-
-    for (let i = 0; i < maxParagraphs; i++) {
-      const beginRect =
-        i < beginSection.paragraphs.length
-          ? getParagraphRect(beginSection.paragraphs[i])
-          : getParagraphRect(
-              endSection.paragraphs[endSection.paragraphs.length - 1]
-            );
-      const endRect =
-        i < endSection.paragraphs.length
-          ? getParagraphRect(endSection.paragraphs[i])
-          : getParagraphRect(
-              beginSection.paragraphs[beginSection.paragraphs.length - 1]
-            );
-
-      const currentRect = interpolateRect(beginRect, endRect, progress);
-      ctx.fillStyle = '#efefef';
-      ctx.fillRect(
-        currentRect.x - editorContainerRect.x,
-        currentRect.y - editorContainerRect.y,
-        currentRect.w,
-        currentRect.h
-      );
-    }
-
-    ctx.scale(1 / dpr, 1 / dpr);
   }
 
   public destroy() {
