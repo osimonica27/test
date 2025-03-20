ant-arktis/AFFiNE

feat(core): ai thinking

packages/frontend/core/src/blocksuite/ai/chat-panel/message/assistant.ts
@@ -67,10 +67,6 @@ export class ChatMessageAssistant extends WithDisposable(ShadowlessElement) {
   renderContent() {
     const { host, item, isLast, status, error } = this;
 
-    if (isLast && status === 'loading') {
-      return html`<ai-loading></ai-loading>`;
-    }
-
     const state = isLast
       ? status !== 'loading' && status !== 'transmitting'
         ? 'finished'
@@ -140,6 +136,10 @@ export class ChatMessageAssistant extends WithDisposable(ShadowlessElement) {
   }
 
   protected override render() {
+    if (this.status === 'loading') {
+      return html`<ai-thinking></ai-thinking>`;
+    }
+
     return html`
       ${this.renderHeader()}
       <div class="item-wrapper">${this.renderContent()}</div>

packages/frontend/core/src/blocksuite/ai/effects.ts
@@ -21,7 +21,7 @@ import { ActionMakeReal } from './chat-panel/actions/make-real';
 import { ActionMindmap } from './chat-panel/actions/mindmap';
 import { ActionSlides } from './chat-panel/actions/slides';
 import { ActionText } from './chat-panel/actions/text';
-import { AILoading } from './chat-panel/ai-loading';
+import { AiThinking } from './chat-panel/ai-thinking';
 import { ChatPanelChips } from './chat-panel/chat-panel-chips';
 import { ChatPanelInput } from './chat-panel/chat-panel-input';
 import { ChatPanelMessages } from './chat-panel/chat-panel-messages';
@@ -86,7 +86,7 @@ export function registerAIEffects() {
   customElements.define('action-mindmap', ActionMindmap);
   customElements.define('action-slides', ActionSlides);
   customElements.define('action-text', ActionText);
-  customElements.define('ai-loading', AILoading);
+  customElements.define('ai-thinking', AiThinking);
   customElements.define('chat-panel-input', ChatPanelInput);
   customElements.define('chat-panel-messages', ChatPanelMessages);
   customElements.define('chat-panel', ChatPanel);

osimonica27/test

packages/frontend/core/src/blocksuite/ai/chat-panel/message/assistant.ts
@@ -1,6 +1,3 @@
-import '../content/assistant-avatar';
-import '../content/rich-text';
-
 import type { EditorHost } from '@blocksuite/affine/block-std';
 import { ShadowlessElement } from '@blocksuite/affine/block-std';
 import { WithDisposable } from '@blocksuite/affine/global/lit';
@@ -66,10 +63,6 @@ export class ChatMessageAssistant extends WithDisposable(ShadowlessElement) {
 
   renderContent() {
     const { host, item, isLast, status, error } = this;
-
-    if (isLast && status === 'loading') {
-      return html`<ai-loading></ai-loading>`;
-    }
 
     const state = isLast
       ? status !== 'loading' && status !== 'transmitting'
@@ -140,6 +133,10 @@ export class ChatMessageAssistant extends WithDisposable(ShadowlessElement) {
   }
 
   protected override render() {
+    if (this.status === 'loading') {
+      return html`<ai-generating></ai-generating>`;
+    }
+
     return html`
       ${this.renderHeader()}
       <div class="item-wrapper">${this.renderContent()}</div>

packages/frontend/core/src/blocksuite/ai/effects.ts
@@ -21,7 +21,9 @@ import { ActionMakeReal } from './chat-panel/actions/make-real';
 import { ActionMindmap } from './chat-panel/actions/mindmap';
 import { ActionSlides } from './chat-panel/actions/slides';
 import { ActionText } from './chat-panel/actions/text';
+import { AIGenerating } from './chat-panel/ai-generating';
 import { AILoading } from './chat-panel/ai-loading';
+import { AIThinking } from './chat-panel/ai-thinking';
 import { ChatPanelChips } from './chat-panel/chat-panel-chips';
 import { ChatPanelInput } from './chat-panel/chat-panel-input';
 import { ChatPanelMessages } from './chat-panel/chat-panel-messages';
@@ -93,6 +95,8 @@ export function registerAIEffects() {
   customElements.define('action-mindmap', ActionMindmap);
   customElements.define('action-slides', ActionSlides);
   customElements.define('action-text', ActionText);
+  customElements.define('ai-thinking', AIThinking);
+  customElements.define('ai-generating', AIGenerating);
   customElements.define('ai-loading', AILoading);
   customElements.define('chat-panel-input', ChatPanelInput);
   customElements.define('chat-panel-messages', ChatPanelMessages);